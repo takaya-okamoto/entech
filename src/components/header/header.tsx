0ae51dbc258191acfd7e987f41e61c90cc8302d2
takaya-okamoto <okamoto.entaku@gmail.com>
2023-01-03 13:59:52 +0900

    fix header link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Link as ChakraLink } from "@chakra-ui/react";
 import { useRecoilValue } from "recoil";
 import { headerState, timeLineModeState } from "../../stores/recoil";
+import Link from "next/link";
 
 export const Header = (): JSX.Element => {
   const headerMode = useRecoilValue(headerState);
@@ -12,13 +13,19 @@ export const Header = (): JSX.Element => {
       pt={".6rem"}
       pl={"10.5rem"}
     >
-      <Flex fontSize={"20px"}>
-        <Text color={timeLineMode === "e" ? "#F4E92A" : "#17949D"}>e</Text>
-        <Text color={headerMode && timeLineMode === "e" ? "white" : "#F4E92A"}>
-          n
-        </Text>
-        <Text color={timeLineMode === "e" ? "white" : "#17949D"}>tech</Text>
-      </Flex>
+      <Link href={"./timeLine"} passHref>
+        <ChakraLink _hover={{ textDecoration: "none" }}>
+          <Flex fontSize={"20px"}>
+            <Text color={timeLineMode === "e" ? "#F4E92A" : "#17949D"}>e</Text>
+            <Text
+              color={headerMode && timeLineMode === "e" ? "white" : "#F4E92A"}
+            >
+              n
+            </Text>
+            <Text color={timeLineMode === "e" ? "white" : "#17949D"}>tech</Text>
+          </Flex>
+        </ChakraLink>
+      </Link>
     </Flex>
   );
 };

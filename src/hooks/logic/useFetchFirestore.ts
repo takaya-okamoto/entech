7796330c920cdb852e93e9c4f8f4459dc5d734cc
takaya-okamoto <okamoto.entaku@gmail.com>
2023-01-08 20:33:22 +0900

    fix bug

--- src/hooks/logic/useFetchFirestore.ts
+++ src/hooks/logic/useFetchFirestore.ts
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
+import { Falsy } from "utility-types";
 
 export const useFetchFirestore = <T, U>(
   fetchFn: (args: T) => Promise<U>,
-  args?: T
+  args: T | Falsy
 ): { data: U | null } => {
   const [data, setData] = useState<U | null>(null);
   useEffect(() => {
--- src/lib/clientSide/firestore/fetchProfile.ts
+++ src/lib/clientSide/firestore/fetchProfile.ts
@@ -1,10 +1,13 @@
 import { doc, getDoc, getFirestore } from "@firebase/firestore";
 import { ProfileType } from "../../../types/profileType";
+import { initializeApp } from "@firebase/app";
+import { firebaseConfig } from "../../../stores/firebase/firebase";
 
 export const fetchProfile = async (
   id: string
 ): Promise<ProfileType | undefined> => {
-  const firestore = getFirestore();
+  const app = initializeApp(firebaseConfig);
+  const firestore = getFirestore(app);
   const ref = doc(firestore, `profile/${id}`);
   const snapshot = await getDoc(ref);
   return snapshot.exists()

ebc68b2d380f94132bd33d402a73ba82c3b46afd
kyohei-murata <k-murata@dctormate.co.jp>
2023-01-08 13:52:03 +0900

    useColorAssets create

--- src/components/chat/message.tsx
+++ src/components/chat/message.tsx
@@ -1,7 +1,8 @@
 import { Avatar, VStack, Flex, Text, Box } from "@chakra-ui/react";
-import ColorAssets from "hooks/view/useColorAssets";
+import { useColorAssets } from "hooks/view/useColorAssets";
 
 export const Message = (): JSX.Element => {
+  const ColorAssets = useColorAssets();
   return (
     <Flex _hover={{ bgColor: ColorAssets.massageBg }}>
       <Avatar size={"sm"} />

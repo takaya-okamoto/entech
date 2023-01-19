1b7533cfc235304a34a77c9f750e8a9feb11e12d
takaya-okamoto <okamoto.entaku@gmail.com>
2023-01-19 16:57:23 +0900

    fix too many render

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -2,7 +2,7 @@ import { Flex, Text } from "@chakra-ui/react";
 import { ConversationPartner } from "../../components/chat/conversationPartner";
 import { useSetRecoilState } from "recoil";
 import { selectedFooterState, timeLineModeState } from "../../stores/recoil";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useFetchFirestore } from "../../hooks/logic/useFetchFirestore";
 import { fetchChatUsers } from "../../lib/clientSide/firestore/fetchChatUsers";
 import { useMyAccount } from "../../hooks/logic/useMyAccount";
@@ -11,14 +11,23 @@ const Chat = (): JSX.Element => {
   const setSelectedFooter = useSetRecoilState<number>(selectedFooterState);
   const setTimeLineMode = useSetRecoilState<string>(timeLineModeState);
   const { user } = useMyAccount();
-  const info1 = {
-    uid: user?.uid ?? "",
-    path: "uid",
-  };
-  const info2 = {
-    uid: user?.uid ?? "",
-    path: "sendUid",
-  };
+  const uid = useMemo(() => {
+    return user?.uid ?? "";
+  }, [user]);
+
+  const info1 = useMemo(() => {
+    return {
+      uid: uid ?? "",
+      path: "uid",
+    };
+  }, [uid]);
+  const info2 = useMemo(() => {
+    return {
+      uid: uid ?? "",
+      path: "sendUid",
+    };
+  }, [uid]);
+
   const chat1 = useFetchFirestore(fetchChatUsers, info1).data ?? [];
   const chat2 = useFetchFirestore(fetchChatUsers, info2).data ?? [];
   const chatUsers = chat1.concat(chat2);

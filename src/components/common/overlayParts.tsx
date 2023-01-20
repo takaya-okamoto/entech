087222323f37b38546acb2a561325e3b66f670f3
kyohei-murata <kyohei.murata10@gmail.com>
2023-01-21 02:45:32 +0900

    popUpMenuButton adjustment#2

--- src/components/common/overlayParts.tsx
+++ src/components/common/overlayParts.tsx
@@ -1,19 +1,32 @@
 import { Box } from "@chakra-ui/react";
 
 type Props = {
-  isShown: boolean;
+  overlay: boolean;
+  isDarker?: boolean;
 };
 
-export const OverlayBackground = (props: Props): JSX.Element => {
-  return (
+export const OverlayParts = (props: Props): JSX.Element => {
+  return props.isDarker ? (
     <Box
-      display={props.isShown ? "block" : "none"}
+      display={props.overlay ? "block" : "none"}
+      zIndex={"1000"}
       pos="fixed"
       minW="100%"
       minH="100vh"
       left={"0"}
       top={"0"}
-      bgColor="rgba(0, 0, 0, 0.5)"
+      bgColor="rgba(0, 0, 0, 0.1)"
+    />
+  ) : (
+    <Box
+      display={props.overlay ? "block" : "none"}
+      zIndex={"1000"}
+      pos="fixed"
+      minW="100%"
+      minH="100vh"
+      left={"0"}
+      top={"0"}
+      bgColor="rgba(0, 0, 0, 0.3)"
     />
   );
 };

f335963df70a004366f85a51ccaef932d5b851ad
takaya-okamoto <okamoto.entaku@gmail.com>
2023-01-28 02:15:47 +0900

    deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const loadEnv = () => {
+  const envWiseContent = require(`./env/env.production`);
+  Object.entries({ ...envWiseContent }).forEach(([key, val]) => {
+    process.env[key] = val;
+    console.log(`[ENVIRONMENT VARIABLE LOAD] ${key}=${val}`);
+  });
+};
+
+loadEnv();
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,

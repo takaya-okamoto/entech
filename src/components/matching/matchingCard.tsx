3f8d0e6c0e7301d1e61b278dfe173231d2feb5ce
kyohei-murata <kyohei.murata10@gmail.com>
2023-01-27 23:56:45 +0900

    second commit

--- src/components/matching/matchingCard.tsx
+++ src/components/matching/matchingCard.tsx
@@ -7,6 +7,7 @@ import {
   useDisclosure,
   Flex,
   Circle,
+  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import { ProfileType } from "../../types/profileType";
@@ -16,6 +17,9 @@ import { ProfileCard } from "../profile/profileCard";
 import { GeneralModal } from "../common/modal/generalModal";
 import { MyPrDisplay } from "../profile/myPrDisplay";
 import { MatchingProfileCard } from "./matchingProfileCard";
+import { useFetchFirestore } from "../../hooks/logic/useFetchFirestore";
+import { fetchProfile } from "../../lib/clientSide/firestore/fetch/fetchProfile";
+import { useMyAccount } from "../../hooks/logic/useMyAccount";
 
 type Props = {
   data: ProfileType | undefined;
@@ -24,13 +28,15 @@ type Props = {
 export const MatchingCard = (props: Props): JSX.Element => {
   const ColorAssets = useColorAssets();
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const { user } = useMyAccount();
+  const { data } = useFetchFirestore(fetchProfile, user?.uid);
+  const userType = data?.userType ?? "e";
   return (
     <VStack pt={"1rem"} spacing={"4rem"}>
       <Flex
         bgColor={ColorAssets.entechSubBlue}
         h={"28rem"}
-        w={"24rem"}
+        w={"23rem"}
         alignItems={"center"}
         justifyContent={"center"}
         direction={"column"}
@@ -38,13 +44,40 @@ export const MatchingCard = (props: Props): JSX.Element => {
         ml={"1px"}
         pt={"1rem"}
       >
-        <HStack>
+        <HStack position={"relative"}>
           <Avatar size={"xl"} src={props.data?.profileImage ?? ""} />
-          <Flex direction={"column"} pl={"2rem"} pt={"2rem"}>
+          <Circle
+            position={"absolute"}
+            size={"40px"}
+            top={"65px"}
+            left={"65px"}
+            bgColor={ColorAssets.entechMainBlue}
+          >
+            {userType === "e" ? (
+              <HStack spacing={"2px"} pb={"8px"} pr={"4px"}>
+                <Text color={ColorAssets.yellow} fontSize={"44px"}>
+                  e
+                </Text>
+                <Text color={ColorAssets.white} pt={"18px"}>
+                  n
+                </Text>
+              </HStack>
+            ) : (
+              <HStack spacing={"2px"} pb={"8px"} pl={"2px"}>
+                <Text color={ColorAssets.white} pt={"18px"}>
+                  e
+                </Text>
+                <Text color={ColorAssets.yellow} fontSize={"44px"}>
+                  n
+                </Text>
+              </HStack>
+            )}
+          </Circle>
+          <Flex direction={"column"} pl={"1rem"} pt={"2rem"}>
             <MatchingMainText text={props.data?.name ?? ""} isBold />
             <Flex>
-              <MatchingMainText text={props.data?.school.faculty ?? ""} />
               <MatchingMainText text={props.data?.school.name ?? ""} />
+              <MatchingMainText text={props.data?.school.faculty ?? ""} />
               <MatchingMainText text={props.data?.school.grade ?? ""} />
             </Flex>
           </Flex>
